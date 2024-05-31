7d117a47ba64d323e80f1dd7d2f4a41e65b66d57
RakibHossen678 <rh491464@gmail.com>
2024-05-31 12:07:28 +0600

    addd guest statistics page

--- src/pages/Dashboard/Common/Statistics.jsx
+++ src/pages/Dashboard/Common/Statistics.jsx
@@ -1,9 +1,19 @@
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
 import useRoles from "../../../hooks/useRoles";
 import AdminStatistics from "../Admin/AdminStatatistics";
+import GuestStatistics from "../Guest/GuestStatistics";
+import HostStatistics from "../Host/HostStatistics";
 
 const Statistics = () => {
   const [role, isLoading] = useRoles();
-  return <div>{role === "admin" && <AdminStatistics></AdminStatistics>}</div>;
+  if (isLoading) return <LoadingSpinner></LoadingSpinner>;
+  return (
+    <>
+      {role === "admin" && <AdminStatistics></AdminStatistics>}
+      {role === "host" && <HostStatistics></HostStatistics>}
+      {role === "guest" && <GuestStatistics></GuestStatistics>}
+    </>
+  );
 };
 
 export default Statistics;
